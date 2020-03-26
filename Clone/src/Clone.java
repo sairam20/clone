b70f9b5ef895ceedd8a2abec969467e0075bd2b2
HP <HP@192.168.0.6>
2020-03-26 15:30:57 +0530

    Pojo Class

---
+++ Clone/src/Clone.java
@@ -0,0 +1,33 @@
+
+
+public class Clone {
+	int a,b;
+
+	public Clone(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	
+
+}
+
+
+
---
+++ Clone/src/MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+
+	public static void main(String[] args) {
+    
+    Clone c=new Clone(10,20);
+    System.out.println(c.getA());
+    System.out.println(c.getB());
+    
+    }
+
+}
