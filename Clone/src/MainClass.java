bc72c5c79ff318f0b91a42245a19280077acd4c6
HP <HP@192.168.0.6>
2020-03-27 16:35:36 +0530

    Changing the program

--- Clone/src/MainClass.java
+++ Clone/src/MainClass.java
@@ -5,7 +5,14 @@ public class MainClass {
     Clone c=new Clone(10,20);
     System.out.println(c.getA());
     System.out.println(c.getB());
-    
+    if(c.getA()==c.getB())
+    {
+    	System.out.println("Both are same");
+    }
+    else
+    {
+    	System.out.println("Both are different");
+    }
     }
 
 }
