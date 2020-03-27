c81d34e9244c395cd7bf8de1a58f16d3bb411731
HP <HP@192.168.0.6>
2020-03-27 11:18:19 +0530

    Changed the values of a and b

--- Clone/src/MainClass.java
+++ Clone/src/MainClass.java
@@ -2,7 +2,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
     
-    Clone c=new Clone(10,20);
+    Clone c=new Clone(30,20);
     System.out.println(c.getA());
     System.out.println(c.getB());
     
