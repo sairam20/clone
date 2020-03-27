95f63225bdd791e6e337a82ddea4c230c1a16de1
HP <HP@192.168.0.6>
2020-03-27 16:37:39 +0530

    Commiting

--- Clone/src/MainClass.java
+++ Clone/src/MainClass.java
@@ -6,14 +6,7 @@ public class MainClass {
     System.out.println("The values are:");
     System.out.println(c.getA());
     System.out.println(c.getB());
-    if(c.getA()==c.getB())
-    {
-    	System.out.println("Both are same");
-    }
-    else
-    {
-    	System.out.println("Both are different");
-    }
+    
     }
 
 }
